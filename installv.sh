92e10bc3e81246a1b72d637b6f3a55b8fa2b9a25
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2021-02-14 17:24:18 +0800

    Update installv.sh

--- installv.sh
+++ installv.sh
@@ -9,7 +9,6 @@ sed -i 's/\("rpc-whitelist-enabled": \).*/\1false,/g' /etc/transmission-daemon/s
 sed -i 's/\("rpc-authentication-required": \).*/\1true,/g' /etc/transmission-daemon/settings.json
 sed -i 's/\("ratio-limit": \).*/\11,/g' /etc/transmission-daemon/settings.json
 sed -i 's/\("ratio-limit-enabled": \).*/\1true,/g' /etc/transmission-daemon/settings.json
-sed -i 's/\("download-dir": \)".*"/\1"\/usr\/downloads"/g' /etc/transmission-daemon/settings.json
 systemctl start transmission-daemon
 
 #/usr/local/etc/caddy/Caddyfile
